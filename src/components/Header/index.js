7e587c0e4590ec47aab772e286f276990518a1e2
avivashishta29 <avivashishta@gmail.com>
2023-05-15 16:20:07 +0530

    proper navigation done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
